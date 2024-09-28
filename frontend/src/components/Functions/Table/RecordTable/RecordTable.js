825866258640020a227a944cb5f89b2c2da04bc4
mengzhuou <mou30@gatech.edu>
2024-09-28 00:32:24 -0400

    Edited Table Style

--- frontend/src/components/Functions/Table/RecordTable/RecordTable.js
+++ frontend/src/components/Functions/Table/RecordTable/RecordTable.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
 import './RecordTable.css';
 import { getRecords } from '../../../../connector.js';
-
-
-
+import AgGridTable from '../AgGridTable/AgGridTable.js';
 class RecordTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
             records: [],
             columnDefs: [
-                //all attributes: company, name, hobby, importantDate, note, additionalNote, family, birthday, reasonOfKnowing, email, position, phoneNumber
-                { headerName: "Name", field: "name", sortable: true, filter: true, width: 230 },
-                { headerName: "Company", field: "company", sortable: true, filter: true, width: 230 },
-                { headerName: "Hobby", field: "hobby", sortable: true, filter: true, width: 230 },
-                { headerName: "Important Event Date", field: "importantDate", sortable: true, filter: true, width: 230, valueFormatter: this.dateFormatter },
-                { headerName: "Family", field: "family", sortable: true, filter: true, width: 230 }
-            ]
+                { headerName: "Name", field: "name", sortable: true, width: 160 },
+                { headerName: "Company", field: "company", sortable: true, width: 180 },
+                { headerName: "Hobby", field: "hobby", sortable: true, width: 190 },
+                { headerName: "Important Event Date", field: "importantDate", sortable: true, width: 300, valueFormatter: this.dateFormatter },
+                { headerName: "Family", field: "family", sortable: true, width: 300 }
+            ],
+            defaultColDef: { sortable: true, resizable: true }, 
+            domLayout: 'autoHeight',
+            suppressHorizontalScroll: true
         };
     }
 
@@ -36,10 +33,9 @@ class RecordTable extends Component {
         }
     };
 
-    //parse the date format to year-Month-day by get rid of the stuffs after T
     dateFormatter = (params) => {
         const date = new Date(params.value);
-        if(!isNaN(date)) {
+        if (!isNaN(date)) {
             return date.toISOString().split('T')[0];
         }
         return params.value;
@@ -48,11 +44,13 @@ class RecordTable extends Component {
     render() {
         return (
             <div className="body">
-                <div className="RecordPageContainer ag-theme-alpine" style={{ height: 500, width: '100%' }}>
-                    <AgGridReact
+                <div className="RecordPageContainer">
+                    <AgGridTable
                         rowData={this.state.records}
                         columnDefs={this.state.columnDefs}
                         defaultColDef={this.state.defaultColDef}
+                        domLayout={this.state.domLayout}
+                        suppressHorizontalScroll={this.state.suppressHorizontalScroll}
                     />
                 </div>
             </div>
