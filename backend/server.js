3b8bcfe42087bd4f731c184afb4c2ac594171843
mengzhuou <mou30@gatech.edu>
2024-11-22 13:14:39 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,17 @@ const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 const corsOptions = require("./config/corsOptions");
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-  });
-}
+if (process.env.NODE_ENV === 'production') {
+  app.get("/*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
 
 // Connect to the database
 connectDB();
